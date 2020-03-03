008a2a9ba6a769a97ca34c6c6c4dc3f606719886
caijh <caiqizhe@gmail.com>
2020-03-03 22:19:43 +0800

    refactor: mysl-centos

--- roles/mysql-centos/files/check.sh
+++ roles/mysql-centos/files/check.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-# check mysql is installed or not, and compare mysql version
-mysql_server_version=$(rpm -qi mysql-server|grep Version)
-
-#Check if we have got the mysql-server version or not.If got,then we have installed mysql.
-if [ -n "$mysql_server_version" ]
+if [[-x $(command -v mysql)]] 
 then
+    echo -n 'New'
+else
+    mysql_server_version = $(mysql --version | awk '{ print $5 }'|awk -F\, '{ print $1 }')
     if $mysql_server_version == $1
     then
         echo -n "Equal"
     else
         echo -n "Upgrade"
     fi
-else
-    echo -n "New"
 fi
