2c07de2823d056658733cc66c351d2c1710235a1
caijh <caiqizhe@gmail.com>
2020-03-05 15:00:22 +0800

    feat: add env.sh to check ansible is install or not

---
+++ bin/env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# check deploy-tool plugins is installed or not
+
+if [ -z $(command -v ansible) ] 
+then
+    error "Yout need to install ansible..."
+    info "visit https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html to see how to install ansible"
+    exit 1
+fi
