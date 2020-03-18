8d5e0a4bfff50426dbe05374e73a650fe7120ffd
蔡俊辉 <caiqizhe@gmail.com>
2020-03-18 22:44:25 +0800

    docs: 修改注释

--- bin/env.sh
+++ bin/env.sh
@@ -7,7 +7,7 @@ source $base_dir/bin/log.sh
 
 if [ -z $(command -v ansible) ] 
 then
-    error "Yout need to install ansible..."
-    info "visit https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html to see how to install ansible"
+    error "运行自动化部署需要安装ansible"
+    info "请访问 https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html, 查看如何安装ansible."
     exit 1
 fi
