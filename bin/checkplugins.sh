fdd304a333053acc2b79b7641585bad136ba2c9e
蔡俊辉 <caiqizhe@gmail.com>
2020-03-22 11:27:09 +0800

    fix: add install shyaml

--- bin/checkplugins.sh
+++ bin/checkplugins.sh
@@ -3,9 +3,29 @@
 source $(dirname $(cd `dirname $0`;pwd))/bin/env.sh
 source $base_dir/bin/log.sh
 
+function install_shyaml()
+{
+    local current_dir=`pwd`
+    cd $base_dir/plugins/shyaml
+    python setup.py install
+    cd $current_dir
+}
+
 if [ -z $(command -v ansible) ] 
 then
-    error "运行自动化部署需要安装ansible"
-    info "请访问 https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html, 查看如何安装ansible."
-    exit 1
+    warn "运行自动化部署需要安装ansible"
+    info "尝试安装ansible"
+    exec rpm -Uvh --force --nodeps $base_dir/plugins/ansible/package/*rpm
+    local ret=$?
+    if [ $ret -ne 0 ] 
+    then
+        error "安装ansible失败"
+        info "请访问 https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html, 查看如何安装ansible."
+        exit 1
+    fi
+fi
+
+if [ -z $(command -v shyaml) ] 
+then
+    install_shyaml
 fi
