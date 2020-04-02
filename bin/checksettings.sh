98393ef3524864a6d8d4f5523b3300c6612e7f85
caijh <caiqizhe@gmail.com>
2020-04-03 00:00:10 +0800

    feat: add k8s-certs

--- bin/checksettings.sh
+++ bin/checksettings.sh
@@ -5,4 +5,4 @@ source $(dirname $(cd `dirname $0`;pwd))/bin/env.sh
 source $base_dir/bin/log.sh
 
 note "开始一键部署前最好，检查与目标主机的连接是否正常。"
-ansible-playbook -i inventory/hosts playbooks/connection-test.yml
+ansible-playbook -i "$base_dir/inventory/hosts" "$base_dir/playbooks/connection-test.yml"
